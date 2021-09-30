bec9889d392adc5a4392deb69599440e776b61a5
kevin-cortina <86452366+kevin-cortina@users.noreply.github.com>
2021-09-29 19:59:28 -0700

    updated java/ unfinished code

--- javascript.js
+++ javascript.js
@@ -3,11 +3,12 @@ var nextButton = document.getElementById('next-btn')
 var questionContainerEl = document.getElementById ('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsEl = document.getElementById('answer-buttons')
-
+var countDown = document.getElementById("countdown")
+var timerClass = document.getElementsByClassName('timerClass')
+var button2 = document.getElementsByClassName('btn correct')
+//added variables for timer
 
 let shuffledQuestions, currentQuestionIndex
-
-
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
@@ -31,6 +32,7 @@ function setNextQuestion () {
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+//shows the actual questions box?
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -39,20 +41,22 @@ function showQuestion(question) {
         button.classList.add('btn')
         if (answer.correct) {
             button.dataset.correct = answer.correct
-        }
+        } 
         button.addEventListener('click', selectAnswer)
         answerButtonsEl.appendChild(button)
     })
 }
 
+//resets questions at the end of quiz
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsEl.firstChild) {
         answerButtonsEl.removeChild(answerButtonsEl.firstChild)
     }
-}
+  }
 
+//adds the boxes of the answers to the webpage
 function selectAnswer(e) {
 	var selectedButton = e.target
 	var correct = selectedButton.dataset.correct
@@ -64,26 +68,74 @@ function selectAnswer(e) {
 	nextButton.classList.remove('hide')
 	} else {
 		startButton.innerText = 'restart'
-		startButton.classList.remove('hide')
-    }
+    startButton.classList.remove('hide')
+  }
 }
 
+//adds the button to tell whether the answer was right or wrong
 function setStatusClass(element, correct) {
     clearStatusClass(element)
 	if (correct) {
-        element.classList.add('correct')
+    element.classList.add('correct')
     } else { 
-        element.classList.add('wrong')
-    }
+    element.classList.add('wrong')
+  }
 }
 
+//adds the answers to the inside of the the answer containers
 function clearStatusClass(element) {
 	element.classList.remove ('correct')
 	element.classList.remove ('wrong')
 }
 
+// starting timer code
+startButton.addEventListener("click", function function1() {
+  let timeleft = 100
+  var downloadTimer = setInterval(function function2(){
+      countDown.innerHTML = timeleft + 
+      "&nbsp"+"seconds remaining";
+      //countdown timer code
+      timeleft -= 1;
+        if(timeleft <= 0) {
+        clearInterval(downloadTimer);
+        countDown.innerHTML = "Time is up!"
+        startButton.innerText = 'restart'
+        startButton.classList.remove('hide')
+        }
+      //pause timer code
+        if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        } else {
+          clearInterval(downloadTimer)
+          console.log(timeleft + 1);
+          }
+        }, 1000);
+  //reset timer code
+  if (startButton.innerText === 'restart') {
+    console.log('this is targeting restart button')
+    startButton.addEventListener('click', function () {
+    clearInterval(downloadTimer);
+    });
+    }
+  //working on penalty code  
+  if (button2 = 'true') {
+    element.classList.addEventListener('click', function () {
+    console.log('right?');
+    
+     });
+  }
+    // button2.addEventListener('click', function () {
+    // timeleft = timeleft - 10 
+    // console.log('targeting wrong answers');
+    // });
+  })
 
 
+
+
+
+
+
+//Quiz Questions
 var questions = [
     {
         question: 'What is 2 + 2?',
@@ -95,12 +147,12 @@ var questions = [
         ]
       },
       {
-        question: 'Who is the best YouTuber?',
+        question: 'What is 5 + 8',
         answers: [
-          { text: 'Web Dev Simplified', correct: true },
-          { text: 'Traversy Media', correct: true },
-          { text: 'Dev Ed', correct: true },
-          { text: 'Fun Fun Function', correct: true }
+          { text: '15', correct: false },
+          { text: '19', correct: false },
+          { text: '13', correct: true },
+          { text: '21', correct: false }
         ]
       },
       {
